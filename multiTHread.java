6a31ea653e88264c85b6ccf788e2f51da561266a
anandukch <anandukch@gmail.com>
2022-02-26 20:44:46 +0530

    multi thread updated

--- multiTHread.java
+++ multiTHread.java
@@ -3,24 +3,30 @@ import java.util.Scanner;
 
 class ThreadA extends Thread {
   int s[];
+  int sum = 0;
 
   ThreadA(int x[]) {
     this.s = x;
+    this.start();
   }
 
   public void run() {
-    int sum = 0;
+
     for (int i = 0; i < s.length; i++) {
-      sum = sum + s[i];
+      this.sum = this.sum + s[i];
     }
-    System.out.println("The sum is " + sum);
-    try {
-      Thread.sleep(2000);
-    } catch (Exception e) {
-      System.out.println(e);
-    }
+    System.out.println("The sum is " + this.sum);
+    // try {
+    // Thread.sleep(2000);
+    // } catch (Exception e) {
+    // System.out.println(e);
+    // }
   }
-  
+
+  public int sum() {
+    return this.sum;
+  }
+
 }
 
 class ThreadDemo {
@@ -33,25 +39,33 @@ class ThreadDemo {
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
     }
-    float a = (n*3)/4;
+    float a = (n * 3) / 4;
     int b = (int) a;
-    int newArr1[] = Arrays.copyOfRange(arr, 0, arr.length/4);
-    int newArr2[] = Arrays.copyOfRange(arr, arr.length/4, arr.length/2);
-    int newArr3[] = Arrays.copyOfRange(arr, arr.length/2, b);
+    int newArr1[] = Arrays.copyOfRange(arr, 0, arr.length / 4);
+    int newArr2[] = Arrays.copyOfRange(arr, arr.length / 4, arr.length / 2);
+    int newArr3[] = Arrays.copyOfRange(arr, arr.length / 2, b);
     int newArr4[] = Arrays.copyOfRange(arr, b, arr.length);
 
     ThreadA T1 = new ThreadA(newArr1);
     ThreadA T2 = new ThreadA(newArr2);
     ThreadA T3 = new ThreadA(newArr3);
     ThreadA T4 = new ThreadA(newArr4);
+    int s1, s2, s3, s4;
     try {
-      T1.start();
-      T2.start();
-      T3.start();
-      T4.start();
+      T1.join();
+      T2.join();
+      T3.join();
+      T4.join();
+
     } catch (Exception e) {
       System.out.println();
     }
+    s1 = T1.sum();
+    s2 = T2.sum();
+    s3 = T3.sum();
+
+    s4 = T4.sum();
+    System.out.println("The sum of the array is " + (s1 + s2 + s3 + s4));
 
   }
 }
